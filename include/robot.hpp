a12d3ed543eb1dbb5438c30031076cc0018072a1
Thierry <enguessthierry@gmail.com>
2026-06-28 22:52:05 +0200

    Added graphics methods to class Robot

--- include/robot.hpp
+++ include/robot.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+#include <memory>
 #include <string>
 #include "message_bus.hpp"
+#include "rounded_rectangle.hpp"
+#include "message_bus.hpp"
 
-class Robot {
+class Robot : public sf::Drawable, public sf::Transformable {
 public:
     Robot() = default;
 
@@ -14,6 +18,13 @@ public:
 
     virtual ~Robot() = default;
 
+    virtual std::unique_ptr<RoundedRectangleShape> createRoundedComponent(
+                    sf::Vector2f size, float radius,
+                    sf::Vector2f origin, sf::Color color) = 0;
+    
+    virtual std::unique_ptr<sf::CircleShape>  createCircleComponent(float radius, 
+                                sf::Vector2f origin, sf::Color color) = 0;
+
     // Update the robot position
     virtual void update(double dt, double simTime) = 0;   // Template Method : design pattern
 
@@ -22,7 +33,10 @@ public:
 
     virtual void reset() {}
 
-    const std::string name() const {return m_name;}
+    const std::string name() const {return m_name;};
+    
+protected:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override = 0;
 
 protected:
     std::string m_name{};
