bf9b6266864c17a9f77abce091afe8b7a1f615f1
Thierry <enguessthierry@gmail.com>
2026-06-29 02:51:35 +0200

    Added documentation in four_wheel_robot.hpp

--- include/four_wheel_robot.hpp
+++ include/four_wheel_robot.hpp
@@ -1,14 +1,34 @@
+/**
+ * @file four_wheel_robot.hpp
+ * @brief Declaration of class FourWheelRobot.
+ *
+ * Description: Declares the variables and methods of class FourWheelRobot.
+ * 
+ * Inheritance:
+ * DifferentialDriveRobot class in order to create four wheel robot object
+ * displayable on a SFML canvas.
+ * -------
+ * @author EnguessT
+ * @date June 29, 2026
+ */
 #pragma once
 
 #include <vector>
 #include <filesystem>
 #include <iostream>
 
-#include "rounded_rectangle.hpp"
 #include "differential_drive.hpp"
 
 class FourWheelRobot : public DifferentialDriveRobot {
 public:
+     /**
+     * @brief Constructs a TwoWheelRobot object
+     * 
+     * @param[in] name the name of the robot
+     * @param[in] initPos: the initial position of the robot
+     * @param[in] color: the color of the robot
+     * @param[in] bus a reference to a message_bus object
+     */
     FourWheelRobot(std::string name, sf::Vector2f positions, 
         sf::Color color, MessageBus& bus);
     
@@ -16,16 +36,21 @@ protected:
      // The draw loop automatically processes the composite system
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
+    /**
+     * @brief create a Circle graphic component
+     */
     std::unique_ptr<sf::CircleShape>  createCircleComponent(float radius, 
-                                sf::Vector2f origin, sf::Color color);
-        
+                                sf::Vector2f origin, sf::Color color) override;
+    
+    /**
+     * @brief create a RoundedRectangle graphic component
+     */ 
     std::unique_ptr<RoundedRectangleShape> createRoundedComponent(
                     sf::Vector2f size, float radius,
-                    sf::Vector2f origin, [[maybe_unused]]sf::Color color);
+                    sf::Vector2f origin, [[maybe_unused]]sf::Color color) override;
        
 
 private:
-    sf::Vector2f m_positions;
     sf::Color m_color;
     sf::Texture m_texture;
     std::vector<std::unique_ptr<sf::Shape>> myShapes;
