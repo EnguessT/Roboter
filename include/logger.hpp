bf3c7de72a28c4268d2a81a6d2e4dada1ce39d5b
Thierry <enguessthierry@gmail.com>
2026-06-29 17:44:25 +0200

    Added some documentation in logger.hpp

--- include/logger.hpp
+++ include/logger.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file logger.hpp
+ * @brief Declaration of variables and methods of class logger
+ *
+ * Description: record all information about subscription messages.
+ * -------
+ * @author EnguessT
+ * @date June 29, 2026
+ */
+
 #pragma once
 #include <iostream>
 
@@ -6,8 +16,13 @@
 
 class Logger {
 public:
+
+    /**
+     * @brief create a Logger object
+     * @param[out] bus: a MessageBus object
+     */
     Logger(MessageBus& bus) {
-        // Sunscribe to the velocity
+        // Subscribe to the velocity
         bus.subscribe<messages::VelocityCommand>(
             [this](const messages::VelocityCommand& msg) {
                 logVelocity(msg);
@@ -30,12 +45,22 @@ public:
     }
 
 private:
+    /**
+     * @brief print velocityCommand infos
+     * @param[out] v: VelocityCommand object reference
+     * @return void
+     */
     void logVelocity(const messages::VelocityCommand& v) {
         std::cout << "[LOG] VelocityCommand from " << v.target 
                   << " left = " << v.left
                   << " right = " << v.right << '\n';
         }
 
+    /**
+     * @brief print Odometry infos
+     * @param[out] o: Odometry object reference
+     * @return void
+     */
     void logOdometry(const messages::Odometry& o) {
         std::cout << "[LOG] Odometry " << o.name
                   << " x = " << o.x
@@ -43,6 +68,11 @@ private:
                   << " theta = " << o.theta << '\n';
     }
 
+    /**
+     * @brief print SimulationTick infos
+     * @param[out] t: SimulationTick object reference
+     * @return void
+     */
     void logTick(const messages::SimulationTick& t) {
         std::cout << "[LOG] Tick time = " << t.time << '\n';
     }
